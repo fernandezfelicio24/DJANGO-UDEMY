b5cb06b0b1ad7d6a6cb29abd3493b448afba6720
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2023-01-17 09:44:23 +0700

    Polish

--- personal_portofolio_project/blog/views.py
+++ personal_portofolio_project/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5]
+    # blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs' : blogs})
 
 
